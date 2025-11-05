0d36c77cbce7a04f763622b37360b2a92e920a92
Randal van Splunteren <randal.van.splunteren@hotmail.com>
2025-11-05 16:41:10 +0100

    Add startup message to CmdletProvider Bump module version to 0.0.2

--- PSCommercetools.Provider/PSCommercetools.Provider.psd1
+++ PSCommercetools.Provider/PSCommercetools.Provider.psd1
@@ -1,6 +1,6 @@
 @{
     RootModule = 'PSCommercetools.Provider.dll'
-    ModuleVersion = '0.0.1'
+    ModuleVersion = '0.0.2'
     GUID = '3d2db01e-a84a-433a-9820-7525b8f0701d'
     FormatsToProcess = 'PSCommercetools.Provider.format.ps1xml'
     Author = 'Randal van Splunteren'
--- PSCommercetools.Provider/PowerShellLayer/CommercetoolsCmdletProvider.cs
+++ PSCommercetools.Provider/PowerShellLayer/CommercetoolsCmdletProvider.cs
@@ -1,3 +1,4 @@
+using System.Management.Automation;
 using System.Management.Automation.Provider;
 using PSCommercetools.Provider.PowerShellLayer.Navigation;
 
@@ -8,4 +9,34 @@ namespace PSCommercetools.Provider.PowerShellLayer;
 [CmdletProvider("PSCommercetools", ProviderCapabilities.Filter | ProviderCapabilities.ShouldProcess)]
 public sealed class CommercetoolsCmdletProvider : CommercetoolsNavigationCmdletProvider
 {
+    protected override ProviderInfo Start(ProviderInfo providerInfo)
+    {
+        if (!SilentStartup())
+        {
+            WriteStartupMessage();
+        }
+
+        return base.Start(providerInfo);
+    }
+
+    private void WriteStartupMessage()
+    {
+        Host.UI.WriteLine(string.Empty);
+        Host.UI.WriteLine("Commercetools PowerShell provider (v0.0.2) started.");
+        Host.UI.WriteLine(string.Empty);
+    }
+
+    private bool SilentStartup()
+    {
+        if (SessionState == null)
+        {
+            return false;
+        }
+
+        PSVariable silentStartupVariable = SessionState.PSVariable.Get("SilentStartup");
+
+        object value = silentStartupVariable?.Value ?? false;
+
+        return value is true;
+    }
 }
